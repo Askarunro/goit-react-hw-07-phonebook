6a875ae54e8fa10e947f61282deda6aa0c69f62a
Askarunro <paskar.y.a@gmail.com>
2022-06-19 16:17:39 +0300

    Update store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,51 +1,51 @@
-import { configureStore } from "@reduxjs/toolkit";
+// import { configureStore } from "@reduxjs/toolkit";
 import { itemsSlice } from "./slice/items";
 import { filterReducer } from "./reduce/filter";
-import storage from "redux-persist/lib/storage";
-import {
-  persistStore,
-  persistReducer,
-  FLUSH,
-  REHYDRATE,
-  PAUSE,
-  PERSIST,
-  PURGE,
-  REGISTER,
-} from "redux-persist";
+// import storage from "redux-persist/lib/storage";
+// import {
+//   persistStore,
+//   persistReducer,
+//   FLUSH,
+//   REHYDRATE,
+//   PAUSE,
+//   PERSIST,
+//   PURGE,
+//   REGISTER,
+// } from "redux-persist";
 
-const persistConfig = {
-  key: "root",
-  storage,
-};
+// const persistConfig = {
+//   key: "root",
+//   storage,
+// };
 
-const persistedReducer = persistReducer(persistConfig, itemsSlice.reducer);
-
-export const store = configureStore({
-  reducer: {
-    items: persistedReducer,
-    filter: filterReducer,
-  },
-  middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware({
-      serializableCheck: {
-        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-      },
-    }),
-});
-
-export const persistor = persistStore(store);
-
-// export const add = createAction("items/add");
-// export const remove = createAction("items/remove");
-// const itemsReducer = createReducer([], {
-//   [add]: (state, action) => [...state, action.payload],
-//   [remove]: (state, action) =>
-//     state.filter((item) => item.name !== action.payload),
-// });
+// const persistedReducer = persistReducer(persistConfig, itemsSlice.reducer);
 
 // export const store = configureStore({
 //   reducer: {
-//     items: itemsReducer,
+//     items: persistedReducer,
 //     filter: filterReducer,
 //   },
+//   middleware: (getDefaultMiddleware) =>
+//     getDefaultMiddleware({
+//       serializableCheck: {
+//         ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+//       },
+//     }),
 // });
+
+// export const persistor = persistStore(store);
+
+import { configureStore } from '@reduxjs/toolkit';
+import { contactsApi } from './api/contactsApi';
+
+export const store = configureStore({
+  reducer: {
+    items: itemsSlice.reducer,
+    filter: filterReducer,
+    [contactsApi.reducerPath]: contactsApi.reducer,
+  },
+  middleware: getDefaultMiddleware => [
+    ...getDefaultMiddleware(),
+    contactsApi.middleware,
+  ],
+});
