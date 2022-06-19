a96c96daea07fe9f03f7daf71765e653528f29e6
Askarunro <paskar.y.a@gmail.com>
2022-06-19 17:11:50 +0300

    Delete filter.js

--- src/redux/reduce/filter.js
+++
@@ -1,6 +0,0 @@
-import { createAction, createReducer} from "@reduxjs/toolkit";
-
-export const searchContact = createAction("filter/searchContact");
-export const filterReducer = createReducer("", {
-  [searchContact]: (state, action) => (state = action.payload),
-});
