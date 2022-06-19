65b672304c67d625b97d491ebb6dc61108c900e4
Askarunro <paskar.y.a@gmail.com>
2022-06-19 17:12:00 +0300

    Update store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,47 +1,8 @@
-// import { configureStore } from "@reduxjs/toolkit";
-import { itemsSlice } from "./slice/items";
-import { filterReducer } from "./reduce/filter";
-// import storage from "redux-persist/lib/storage";
-// import {
-//   persistStore,
-//   persistReducer,
-//   FLUSH,
-//   REHYDRATE,
-//   PAUSE,
-//   PERSIST,
-//   PURGE,
-//   REGISTER,
-// } from "redux-persist";
-
-// const persistConfig = {
-//   key: "root",
-//   storage,
-// };
-
-// const persistedReducer = persistReducer(persistConfig, itemsSlice.reducer);
-
-// export const store = configureStore({
-//   reducer: {
-//     items: persistedReducer,
-//     filter: filterReducer,
-//   },
-//   middleware: (getDefaultMiddleware) =>
-//     getDefaultMiddleware({
-//       serializableCheck: {
-//         ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-//       },
-//     }),
-// });
-
-// export const persistor = persistStore(store);
-
 import { configureStore } from '@reduxjs/toolkit';
 import { contactsApi } from './api/contactsApi';
 
 export const store = configureStore({
   reducer: {
-    items: itemsSlice.reducer,
-    filter: filterReducer,
     [contactsApi.reducerPath]: contactsApi.reducer,
   },
   middleware: getDefaultMiddleware => [
